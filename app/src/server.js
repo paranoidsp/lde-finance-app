eba0ebefd15ad1d18a372d46e8586475f8745cdf
Karthikeya Viswanath <viswanathkarthikeya@gmail.com>
2017-08-08 23:07:19 +0530

    Init

---
+++ app/src/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug')
+
+//your routes here
+app.get('/', function (req, res) {
+    res.render('index')
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
